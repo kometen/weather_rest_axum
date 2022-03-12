79fe0ea6a3c31ce555acf1ffddaf7dff5dfbbb1e
Claus Guttesen <kometen@gmail.com>
2022-03-12 18:50:46 +0100

    Removed print-statements.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use tokio_postgres::types::{Date, Timestamp, FromSql};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use rust_decimal::prelude::*;
 
+#[derive(Debug)]
 pub struct MeasurementsSingleLocation {
     id: i32,
     name: String,
@@ -97,38 +98,6 @@ async fn using_connection_pool_extractor(
         .map(|row| MeasurementsSingleLocation::try_from(&row).unwrap())
         .collect();
 
-    /*
-    for r in &rows {
-        let tg_name = r.try_get(1);
-        let name = match tg_name {
-            Ok(tg_name) => tg_name,
-            Err(_) => "Invalid site"
-        };
-        println!("name: {}", name);
-
-        let tg_latitude = r.try_get(2);
-        let latitude = match tg_latitude {
-            Ok(tg) => Decimal::from_str(tg),
-            Err(_) => Decimal::from_str("-1.0")
-        };
-        let latitude = latitude.unwrap();
-        println!("latitude: {}", latitude);
-
-        let tg_longitude = r.try_get(3);
-        let longitude = match tg_longitude {
-            Ok(tg) => Decimal::from_str(tg),
-            Err(_) => Decimal::from_str("-1.0")
-        };
-        let longitude = longitude.unwrap();
-        println!("latitude: {}", longitude);
-
-        let tg_measurement_time_default = r.try_get(4);
-        println!("measurement_time_default: {}", tg_measurement_time_default.unwrap());
-
-    }*/
-
-    //let two = rows.get(0);
-
     Ok("2".to_string())
 }
 
@@ -148,41 +117,36 @@ impl<'a> TryFrom<&'a Row> for MeasurementsSingleLocation {
             Ok(id) => id,
             Err(_) => -1
         };
-        println!("id: {}", &id);
+
         let tg_name = row.try_get("name");
         let name = match tg_name {
             Ok(tg) => tg,
             Err(_) => "unknown site".to_string()
         };
-        println!("name: {}", &name);
 
         let tg_latitude = row.try_get("latitude");
         let latitude = match tg_latitude {
             Ok(tg) => tg,
             Err(_) => "-0.1".to_string()
         };
-        println!("latitude: {}", &latitude);
 
         let tg_longitude = row.try_get("longitude");
         let longitude = match tg_longitude {
             Ok(tg) => tg,
             Err(_) => "-0.1".to_string()
         };
-        println!("longitude: {}", &longitude);
 
         let tg_measurement_time_default = row.try_get("measurement_time_default");
         let measurement_time_default= match tg_measurement_time_default {
             Ok(tg) => tg,
             Err(_) => Utc.ymd(1967, 10, 28).and_hms(10, 11, 12)
         };
-        println!("measurement_time_default: {}", measurement_time_default);
 
         let measurements = row.try_get("measurements").unwrap();
         /*let measurements = match tg_measurements {
             Ok(tg) => tg,
             Err(_) => serde_json::from_str(r#"[{"invalid":true]"#)
         };*/
-        println!("measurements: {}", measurements);
 
         Ok(Self {
             id,
